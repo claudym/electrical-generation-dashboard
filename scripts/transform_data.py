2b149dad6ca464a30867fa260bd0550c0bcb268a
Claudy <claudy.m.mejia@gmail.com>
2024-05-30 21:22:09 -0400

    Modify to save in dynamodb table

--- scripts/transform_data.py
+++ scripts/transform_data.py
@@ -4,7 +4,13 @@ from datetime import datetime, timedelta
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+import boto3
+from decimal import Decimal
 
+
+TABLE_NAME = 'basic_electrical_generation_data_dom_rep'
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(TABLE_NAME)
 
 NAMESPACE_UUID = uuid.NAMESPACE_URL
 
@@ -16,11 +22,10 @@ def transform(input_object):
     transformed_objects = []
     base_time = datetime.strptime(input_object['FECHA'], "%Y-%m-%dT%H:%M:%S")
     
-    # Handle H1 to H23 normally
     for hour in range(1, 24):
         new_time = base_time + timedelta(hours=hour)
         datetime_str = new_time.strftime("%Y-%m-%dT%H:%M:%S")
-        energy_value = input_object[f'H{hour}']
+        energy_value = Decimal(str(input_object[f'H{hour}']))
         item_id = generate_uuid(input_object['GRUPO'], input_object['EMPRESA'], input_object['CENTRAL'], datetime_str)
         transformed_objects.append({
             "id": item_id,
@@ -35,7 +40,7 @@ def transform(input_object):
     # Handle H24 for the next day
     next_day_time = base_time + timedelta(days=1)
     datetime_str = new_time.strftime("%Y-%m-%dT%H:%M:%S")
-    energy_value = input_object['H24']
+    energy_value = Decimal(str(input_object['H24']))
     item_id = generate_uuid(input_object['GRUPO'], input_object['EMPRESA'], input_object['CENTRAL'], datetime_str)
     transformed_objects.append({
         "id": item_id,
@@ -74,15 +79,13 @@ def fetch_data_from_api(url, retries=3, backoff_factor=0.3, timeout=10):
         return None
 
 def main():
-    start_date = '2024-05-01'
-    end_date = '2024-05-02'
+    start_date = '2013-01-01'
+    end_date = '2013-01-01'
     
     start_date_obj = datetime.strptime(start_date, '%Y-%m-%d')
     end_date_obj = datetime.strptime(end_date, '%Y-%m-%d')
     
     current_date_obj = start_date_obj
-
-    all_transformed_data = []
 
     while current_date_obj <= end_date_obj:
         current_date_str = current_date_obj.strftime('%m/%d/%Y')
@@ -93,14 +96,13 @@ def main():
             input_data = data["GetPostDespacho"]
             
             for item in input_data:
-                all_transformed_data.extend(transform(item))
+                transformed_items = transform(item)
+                for transformed_item in transformed_items:
+                    table.put_item(Item=transformed_item)
         
         current_date_obj += timedelta(days=1)
     
-    with open('transformed_data.json', 'w', encoding='utf-8') as outfile:
-        json.dump(all_transformed_data, outfile, indent=4, ensure_ascii=False)
-
-    print("Data transformation complete. Output written to transformed_data.json.")
+    print("Data transformation and upload complete.")
 
 if __name__ == "__main__":
     main()
