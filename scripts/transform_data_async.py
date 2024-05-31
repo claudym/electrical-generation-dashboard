84b687af3d9eefbd641ca6f7316ec928e5d9a150
Claudy <claudy.m.mejia@gmail.com>
2024-05-31 14:56:15 -0400

    Modify main script to transform items parallely

--- scripts/transform_data_async.py
+++ scripts/transform_data_async.py
@@ -7,7 +7,6 @@ from decimal import Decimal
 import asyncio
 from tenacity import retry, wait_exponential, stop_after_attempt, retry_if_exception_type
 import aiohttp.client_exceptions
-from concurrent.futures import ProcessPoolExecutor
 
 
 TABLE_NAME = 'basic_electrical_generation_data_dom_rep'
@@ -18,7 +17,7 @@ def generate_uuid(group, company, plant, datetime_str):
     return str(uuid.uuid5(NAMESPACE_UUID, unique_string))
 
 def transform(input_object):
-    transformed_objects = []
+    transformed_objects = {}
     base_time = datetime.strptime(input_object['FECHA'], "%Y-%m-%dT%H:%M:%S")
     
     for hour in range(1, 24):
@@ -26,7 +25,7 @@ def transform(input_object):
         datetime_str = new_time.strftime("%Y-%m-%dT%H:%M:%S")
         energy_value = Decimal(str(input_object[f'H{hour}']))
         item_id = generate_uuid(input_object['GRUPO'], input_object['EMPRESA'], input_object['CENTRAL'], datetime_str)
-        transformed_objects.append({
+        transformed_objects[item_id] = {
             "id": item_id,
             "group": input_object['GRUPO'],
             "group_plant": f"{input_object['GRUPO']}-{input_object['CENTRAL']}",
@@ -34,14 +33,14 @@ def transform(input_object):
             "plant": input_object['CENTRAL'],
             "datetime": new_time.isoformat(),
             "energy": energy_value
-        })
+        }
     
     # Handle H24 for the next day
     next_day_time = base_time + timedelta(days=1)
     datetime_str = next_day_time.strftime("%Y-%m-%dT%H:%M:%S")
     energy_value = Decimal(str(input_object['H24']))
     item_id = generate_uuid(input_object['GRUPO'], input_object['EMPRESA'], input_object['CENTRAL'], datetime_str)
-    transformed_objects.append({
+    transformed_objects[item_id] = {
         "id": item_id,
         "group": input_object['GRUPO'],
         "group_plant": f"{input_object['GRUPO']}-{input_object['CENTRAL']}",
@@ -49,7 +48,7 @@ def transform(input_object):
         "plant": input_object['CENTRAL'],
         "datetime": next_day_time.isoformat(),
         "energy": energy_value
-    })
+    }
     
     return transformed_objects
 
@@ -64,36 +63,27 @@ async def fetch_data_from_api(url, session):
         raise
 
 async def batch_write_items(table, items, batch_size=25):
-    unique_items = {}
-    for item in items:
-        key = item['id']
-        unique_items[key] = item
-
-    items = list(unique_items.values())
-
     async with table.batch_writer() as batch:
         for i in range(0, len(items), batch_size):
             for item in items[i:i + batch_size]:
                 await batch.put_item(Item=item)
 
-async def process_date(http_session, current_date_str, dynamodb, executor):
+async def process_date(http_session, current_date_str, dynamodb):
     api_url = f'https://apps.oc.org.do/wsOCWebsiteChart/Service.asmx/GetPostDespachoJSon?Fecha={current_date_str}'
     data = await fetch_data_from_api(api_url, http_session)
     if data:
         input_data = data["GetPostDespacho"]
-        loop = asyncio.get_running_loop()
-        transformed_items_list = await loop.run_in_executor(executor, transform_parallel, input_data)
-        all_transformed_items = [item for sublist in transformed_items_list for item in sublist]
+        all_transformed_items = {}
+        for item in input_data:
+            transformed_items = transform(item)
+            all_transformed_items.update(transformed_items)
         
         table = await dynamodb.Table(TABLE_NAME)
-        await batch_write_items(table, all_transformed_items)
-
-def transform_parallel(input_data):
-    return [transform(item) for item in input_data]
+        await batch_write_items(table, list(all_transformed_items.values()))
 
 async def main():
-    start_date = '2020-01-07'
-    end_date = '2020-01-08'
+    start_date = '2013-01-28'
+    end_date = '2013-02-01'
     
     start_date_obj = datetime.strptime(start_date, '%Y-%m-%d')
     end_date_obj = datetime.strptime(end_date, '%Y-%m-%d')
@@ -103,16 +93,14 @@ async def main():
 
     session = aioboto3.Session()
 
-    with ProcessPoolExecutor() as executor:
-        async with aiohttp.ClientSession() as http_session:
-            async with session.resource('dynamodb') as dynamodb:
-                while current_date_obj <= end_date_obj:
-                    current_date_str = current_date_obj.strftime('%m/%d/%Y')
-                    tasks.append(process_date(http_session, current_date_str, dynamodb, executor))
-                    current_date_obj += timedelta(days=1)
-            
-                await asyncio.gather(*tasks)
-    
+    async with aiohttp.ClientSession() as http_session:
+        async with session.resource('dynamodb') as dynamodb:
+            while current_date_obj <= end_date_obj:
+                current_date_str = current_date_obj.strftime('%m/%d/%Y')
+                tasks.append(process_date(http_session, current_date_str, dynamodb))
+                current_date_obj += timedelta(days=1)
+        
+            await asyncio.gather(*tasks)
     print("Data transformation and upload complete.")
 
 if __name__ == "__main__":
